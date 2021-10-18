61925dcb9612d49491d9176de7837ec92be431b0
ahbadr257 <ahbadr257@gmail.com>
2021-10-18 11:31:46 +0200

    Initial commit

---
+++ app/src/main/java/testproject/aaamab/startedwithandroid/ui/login/LoginScreen.java
@@ -0,0 +1,16 @@
+package testproject.aaamab.startedwithandroid.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import testproject.aaamab.startedwithandroid.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
---
+++ app/src/main/java/testproject/aaamab/startedwithandroid/ui/splash/SplashScreen.java
@@ -0,0 +1,16 @@
+package testproject.aaamab.startedwithandroid.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import testproject.aaamab.startedwithandroid.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
